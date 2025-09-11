3744e0fbaec109f3ea39f93177b6bf44e39e2dc9
carlossecato <carlos.secato@gmail.com>
2025-09-10 22:15:32 -0300

    initial commit - spring initializer

---
+++ src/test/java/com/ngbank/ngbank_api/NgbankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngbank.ngbank_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgbankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
